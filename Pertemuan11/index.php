871277a7f73c7199d48708a70fee6c442faed472
minoritas13 <nafisrizqullah12@gmail.com>
2025-05-08 15:28:46 +0700

    push login

---
+++ Pertemuan11/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="tampil.php" method="post">
+        <label for="nama"> masukan nama </label>
+        <div>
+            <input type="text" name="nama">
+        </div>
+        <label for="nama"> usia </label>
+        <div>
+            <input type="text" name="usia">
+        </div>
+        <label for="nama"> NPM </label>
+        <div>
+            <input type="text" name="npm">
+        </div>
+        <label for="nama"> alamat </label>
+        <div>
+            <input type="text" name="alamat">
+        </div>
+        <input type="submit">
+    </form>
+</body>
+</html>
---
+++ Pertemuan11/tampil.php
@@ -0,0 +1,12 @@
+<?php
+
+    $nama = $_POST['nama'];
+    $usia = $_POST['usia'];
+    $npm = $_POST['npm'];
+    $alamat = $_POST['alamat'];
+    
+    echo $nama; 
+    echo $usia;
+    echo $npm;
+    echo $alamat;
+?>
