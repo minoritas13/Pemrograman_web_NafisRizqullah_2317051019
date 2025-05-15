c99ab382ead7e61d5addb9b1012ffb2cfa062391
minoritas13 <nafisrizqullah12@gmail.com>
2025-05-15 15:05:36 +0700

    push pertemuan12

---
+++ Pertemuan12/koneksi/db.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "latihan_login_db";
+
+$conn = mysqli_connect($host,$user,$password,$db);
+
+if(!$conn){
+    die("koneksi gagal".mysqli_connect_error());
+};
+
+?>
